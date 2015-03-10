e5966c9cee3fc6e41435bfdff3a8a7cda02da337
Adrian Perez <perezA@appacademy.us>
2015-03-10 13:13:09 -0700

    kiub

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -10,10 +10,21 @@ game.PlayerEntity = me.Entity.extend({
                     return(new me.Rect(0, 0, 64, 64)).toPolygon();
                 }
         }]);
+    
+         this.body.setVelocity(5, 20);
         
     },
     
-    update: function(){
-        
+    update: function(delta){
+        if(me.input.isKeyPressed("right")) {
+            // setting the position of X 
+            //setVelocity is being mutiplied by me.timer.tick;
+            //me.timer.tick is making the character move smoothly
+            this.body.vel.x += this.body.accel.x * me.timer.tick;
+        }else{
+            this.body.vel.x = 0;
+        }
+        this.body.update(delta);
+        return true;
     }
 });
