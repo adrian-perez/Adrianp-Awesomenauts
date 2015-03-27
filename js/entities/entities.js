ffd0fd1d355c05cfa44a9a933d493bff3987a5e2
Adrian Perez <perezA@appacademy.us>
2015-03-27 12:17:54 -0700

    khv

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -201,7 +201,10 @@ game.EnemyCreep = me.Entity.extend({
         }]);  
     this.health = 10;
     this.alwaysUpdate = true;
-    
+    //  this.attacking lets us know if the enemy is currently attacking
+    this.attacking = false;
+    // code here 
+    this.now = new Dtae().getTime;
     this.body.setVelocity(3, 20);
     
     this.type = "EnemyCreep";
@@ -212,8 +215,29 @@ game.EnemyCreep = me.Entity.extend({
     },
     
     update: function(delta){
+       
+         this.body.vel.x -= this.body.accel.x * me.timer.tick;
+        
+         me.collision.check(this, true, this.coliisionHandler.bind(this), true);
+       
+         this.body.update(delta);
+
+        this._super(me.Entity, "update", [delta]);
      
         return true;
+    },
+    
+    collisionHandler: function(response){
+        if(response.b.type==='PlayerBase'){
+            this.attacking=true;
+            this.lastAttacking=this.now;
+            this.body.vel.x = 0;
+            this.pos.x = this.pos.x + 1;
+            if((this.now-this.lastHit >= 1000)){
+                this.latsHit = this.now;
+                response.b.loseHealth(1);
+            }
+        }
     }
     
 });
