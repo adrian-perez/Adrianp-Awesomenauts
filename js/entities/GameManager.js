a047a88c5bf3105a0cc6219e4b5e6450c9a9c0aa
Adrian Perez <perezA@appacademy.us>
2015-04-20 21:14:29 -0700

    fgk,

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -6,23 +6,21 @@ game.GameTimerManager = Object.extend({
         this.alwaysUpdate = true;
     },
     update: function() {
-        this.now = new Date().getTime();       
+        this.now = new Date().getTime();
         this.goldTimerCheck();
         this.creepTimerCheck();
 
         return true;
-    }, 
-
-    goldTimerCheck: function(){
-         if (Math.round(this.now/1000) % 20 === 0 && ((this.now - this.lastCreep) >= 1000)) {
-             game.data.gold += 1;
-             console.log("Curret gold: " + game.data.gold);
-              // console.log(this.now + " " + this.lastCreep);
+    },
+    goldTimerCheck: function() {
+        if (Math.round(this.now / 1000) % 20 === 0 && ((this.now - this.lastCreep) >= 1000)) {
+            game.data.gold += 1;
+            console.log("Curret gold: " + game.data.gold);
+            // console.log(this.now + " " + this.lastCreep);
         }
     },
-    
-    creepTimerCheck: function(){
-          if (Math.round(this.now / 1000) % 10 === 0 && ((this.now - this.lastCreep) >= 1000)) {
+    creepTimerCheck: function() {
+        if (Math.round(this.now / 1000) % 10 === 0 && ((this.now - this.lastCreep) >= 1000)) {
             this.lastCreep = this.now;
             var creepe = me.pool.pull("EnemyCreep", 1000, 0, {});
             me.game.world.addChild(creepe, 5);
@@ -31,53 +29,50 @@ game.GameTimerManager = Object.extend({
 });
 
 game.HeroDeathManager = Object.extend({
-    init: function(x, y, settings){
-      this.alwaysUpdate = true;
+    init: function(x, y, settings) {
+        this.alwaysUpdate = true;
 
     },
-    
-    update: function(){
-         if (game.data.player.dead) {
+    update: function() {
+        if (game.data.player.dead) {
             me.game.world.removeChild(game.data.player);
             me.state.current().resetPlayer(10, 0);
 
         }
-        
+
         return true;
     }
 });
 
 game.ExperienceManager = Object.extend({
-    init: function(x, y, settings){
+    init: function(x, y, settings) {
         this.alwaysUpdate = true;
         this.gameover = false;
     },
-    
-    update: function(){
-        if(game.data.win === true && !this.gameover){
+    update: function() {
+        if (game.data.win === true && !this.gameover) {
             this.gameOver(true);
-        }else if(game.data.win === false && !this.gameOver){
-           this.gameOver(false);
+        } else if (game.data.win === false && !this.gameOver) {
+            this.gameOver(false);
         }
-        
+
         return true;
     },
-    
-    gameOver: function(win){
-        if(win){
+    gameOver: function(win) {
+        if (win) {
             game.data.exp += 10;
-        }else{
-         game.data.exp += 1;
+        } else {
+            game.data.exp += 1;
         }
         this.gameover = true;
         me.save.exp = game.data.exp;
-       
+
     }
-    
+
 });
 
 game.SpendGold = Object.extend({
-    init: function(){
+    init: function() {
         this.now = new Date().getTime();
         this.lastBuy = new Date().getTime();
         this.paused = false;
@@ -85,30 +80,31 @@ game.SpendGold = Object.extend({
         this.updateWhenPaused = true;
         this.buying = false;
     },
-    
-    update: function(){
+    update: function() {
         this.now = new Date().getTime();
-
-        if(me.input.isKeyPressed("buy") && this.now-this.lastbuy>=1000){
+        console.log(this.buying);
+        if (me.input.isKeyPressed("buy") && this.now - this.lastBuy >= 1000) {
             this.lastBuy = this.now;
-            if(!this.buying)
+            if (!this.buying) {
+
                 this.startBuying();
-        }else{
-            this.stopBuying();
+            } else {
+                this.stopBuying();
+            }
         }
-        
-       this.checkBuyKeys();
-        
-       return true;
+
+        this.checkBuyKeys();
+
+        return true;
     },
-    
-    startBuying: function(){
+    startBuying: function() {
+        console.log("startbuy");
         this.buying = true;
         me.state.pause(me.state.PLAY);
         game.data.pausePos = me.game.viewport.localToWorld(0, 0);
         game.data.buyscreen = new me.Sprite(game.data.pausePos.x, game.data.pausePos.y, me.loader.getImage('gold-screen'));
         game.data.buyscreen.updateWhenPaused = true;
-        game.data.buyscreen.setOpacity(0, 8);
+        game.data.buyscreen.setOpacity(0.8);
         me.game.world.addChild(game.data.buyscreen, 34);
         game.data.player.body.setVelocity(0, 0);
         me.state.pause(me.state.PLAY);
@@ -120,9 +116,8 @@ game.SpendGold = Object.extend({
         me.input.bindKey(me.input.KEY.F1, "F1", true);
         this.setBuyText();
     },
-    
-    setBuyText: function(){
-       game.data.buyText = new (me.Renderable.extend({
+    setBuyText: function() {
+        game.data.buyText = new (me.Renderable.extend({
             init: function() {
                 this._super(me.Renderable, 'init', [game.data.pausePos.x, game.data.pausePos.y, 270, 240, 300, 50]);
                 this.font = new me.Font("Graffiti", 26, "white");
@@ -131,20 +126,20 @@ game.SpendGold = Object.extend({
             },
             draw: function(renderer) {
                 this.font.draw(renderer.getContext(), "PRESS F1-F6 TO BUY, F5 TO SKIP", this.pos.x, this.pos.y);
-                this.font.draw(renderer.getContext(), "Skill1: Increase Damage. Current Level: " + game.data.skill1 + " Cost: " + ((game.data.skill1+1)*10), this.pos.x, this.pos.y);
-                this.font.draw(renderer.getContext(), "Skill2: Run Faster! CurrentLevel: " + game.data.skill2 + " Cost: " + ((game.data.skill2+1)*10), this.pos.x, this.pos.y);
-                this.font.draw(renderer.getContext(), "Skill3: Increase Health! CurrentLevel: " + game.data.skill3 + " Cost: " + ((game.data.skill3+1)*10), this.pos.x, this.pos.y);
-                this.font.draw(renderer.getContext(), "Q Ability: Speed Burst. CurrentLevel: " + game.data.ability1 + " Cost: " + ((game.data.ability1+1)*10), this.pos.x, this.pos.y);
-                this.font.draw(renderer.getContext(), "W Ability: Eat your crrep for health: " + game.data.ability2 + " Cost: " + ((game.data.ability2+1)*10), this.pos.x, this.pos.y);
-                this.font.draw(renderer.getContext(), "E Ability: Throw your spear: " + game.data.ability3 + " Cost: " + ((game.data.ability3+1)*10), this.pos.x, this.pos.y);v
-              
+                this.font.draw(renderer.getContext(), "Skill1: Increase Damage. Current Level: " + game.data.skill1 + " Cost: " + ((game.data.skill1 + 1) * 10), this.pos.x, this.pos.y + 50);
+                this.font.draw(renderer.getContext(), "Skill2: Run Faster! CurrentLevel: " + game.data.skill2 + " Cost: " + ((game.data.skill2 + 1) * 10), this.pos.x, this.pos.y + 100);
+                this.font.draw(renderer.getContext(), "Skill3: Increase Health! CurrentLevel: " + game.data.skill3 + " Cost: " + ((game.data.skill3 + 1) * 10), this.pos.x, this.pos.y + 150);
+                this.font.draw(renderer.getContext(), "Q Ability: Speed Burst. CurrentLevel: " + game.data.ability1 + " Cost: " + ((game.data.ability1 + 1) * 10), this.pos.x, this.pos.y + 200);
+                this.font.draw(renderer.getContext(), "W Ability: Eat your crrep for health: " + game.data.ability2 + " Cost: " + ((game.data.ability2 + 1) * 10), this.pos.x, this.pos.y + 250);
+                this.font.draw(renderer.getContext(), "E Ability: Throw your spear: " + game.data.ability3 + " Cost: " + ((game.data.ability3 + 1) * 10), this.pos.x, this.pos.y + 300);
+
+
             }
 
         }));
-    me.game.world.addChild();
+        me.game.world.addChild(game.data.buyText, 35);
     },
-    
-    stopBuying: function(){
+    stopBuying: function() {
         this.buying = false;
         me.state.resume(me.state.PLAY);
         game.data.player.body.setVelocity(game.data.playerMoveSpeed, 20);
@@ -157,8 +152,45 @@ game.SpendGold = Object.extend({
         me.input.unbindKey(me.input.KEY.F6, "F6", true);
     },
     
-    checkBuyKeys: function(){
+    checkBuyKeys: function() {
+        if (me.input.isKeyPressed("F1")) {
+            if (this.checkCost(1)) {
+                this.makePurchase(1);
+            }
+        } else if (me.input.isKeyPressed("F2")) {
+            if (this.checkCost(2)) {
+                this.makePurchase(2);
+            }
+        } else if (me.input.isKeyPressed("F3")) {
+            if (this.checkCost(3)) {
+                this.makePurchase(3);
+            }
+        } else if (me.input.isKeyPressed("F4")) {
+            if (this.checkCost(4)) {
+                this.makePurchase(4);
+            }
+        } else if (me.input.isKeyPressed("F5")) {
+            if (this.checkCost(5)) {
+                this.makePurchase(5);
+            }
+        } else if (me.input.isKeyPressed("F6")) {
+            if (this.checkCost(6)) {
+                this.makePurchase(6);
+            }
+        }
+    },
+    
+    checkCost: function(skill) {
+        if(skill){
+            return true;
+        }else{
+            return false;
+        }
         
+
+    },
+    makePurchase: function(skill) {
+
     }
-     
+
 });
